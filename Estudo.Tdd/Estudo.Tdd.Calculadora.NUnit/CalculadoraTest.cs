02e6f00be69f04ad5d1cc0767ebcb1411999d1e9
samuel.vaz <samuel.rvg@gmail.com>
2019-07-01 15:45:24 -0300

    test with nunit 3x

---
+++ Estudo.Tdd/Estudo.Tdd.Calculadora.NUnit/CalculadoraTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Estudo.Tdd.Calculadora.NUnit
+{
+    //Instalar extensao para visualizar no vs: Tools -> Extensions And Updates
+    [TestFixture]
+    class CalculadoraTest
+    {
+        private Calculadora _calc;
+
+        [Test]
+        public void TestSomar()
+        {
+            Calculadora calc = new Calculadora();
+
+            Assert.AreEqual(4, calc.Somar(4, 0));
+        }
+
+        [Test]
+        public void TestSubtrair()
+        {
+            Calculadora calc = new Calculadora();
+
+            //Assert.AreEqual(0, calc.Subtrair(2, 2));
+            Assert.IsTrue(calc.Subtrair(2, 2) == 0);
+        }
+    }
+}
